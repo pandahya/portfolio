b4079c64b492c25a938f12c348d68e6b4e0848bc
hyacinth <hyacinthweng@gmail.com>
2022-01-20 21:39:16 -0500

    updated

--- draw.js
+++ draw.js
@@ -1,23 +1,23 @@
 const c = document.getElementById("draw");
-const ctx = c.getContext('2d');
+    const ctx = c.getContext('2d');
 
-window.addEventListener('load', function(){
-    c.addEventListener('mousemove', draw);
-})
-function draw(e){
-    let xPos = event.clientX;
-    let yPos = event.clientY;
+    window.addEventListener('load', function(){
+        window.addEventListener('mousemove', draw);
+    })
+    function draw(e){
+        let xPos = event.clientX;
+        let yPos = event.clientY;
 
-    let canvLocation = c.getBoundingClientRect();
-    let canvX = canvLocation.x;
-    let canvY = canvLocation.y;
+        let canvLocation = c.getBoundingClientRect();
+        let canvX = canvLocation.x;
+        let canvY = canvLocation.y;
 
-    let mouseX = xPos - canvX;
-    let mouseY = yPos - canvY;
+        let mouseX = xPos - canvX;
+        let mouseY = yPos - canvY;
 
-    ctx.beginPath();
-    ctx.arc(mouseX, mouseY, 10, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    console.log('hi');
-}
+        ctx.beginPath();
+        ctx.arc(mouseX, mouseY, 10, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'rgba(255, 215, 0, 0.1)';
+        ctx.fill();
+        console.log('hi');
+    }
