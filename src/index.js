65daca3de34007ff4e9cd9f53b05890c56f454f8
Hyacinth <hyacinthweng@gmail.com>
2023-02-25 00:40:30 -0500

    started on webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+require('file-loader?name=[name].[ext]!../index.html')
+import React from 'react';
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+    output:{
+        entry: path.resolve(__dirname, './src'),
+        filename: 'index.bundle.js',
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+
+};
